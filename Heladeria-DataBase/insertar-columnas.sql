066a20203d5344542ebbae1c61c78fa1d0904e98
Diego Ajata Ledezma <ajataledezmadiego@gmail.com>
2025-06-19 10:47:00 -0300

    Realizamos 4 consultas en sql

--- Heladeria-DataBase/insertar-columnas.sql
+++ Heladeria-DataBase/insertar-columnas.sql
@@ -5,7 +5,7 @@ INSERT INTO especiales (nombre_especial, tienda_id)
 VALUES ("Tramontana", 1), ("Flan", 1);
 
 INSERT INTO salsas (nombre_salsa)
-VALUES ("Salsa de frutilla"),
+VALUES ("Salsa de Frutilla"),
 ("Salsa de Chocolate"),
 ("Salsa de Dulce de Leche"),
 ("Salsa de Caramelo"),
@@ -109,6 +109,7 @@ CROSS JOIN cucuruchos c;
 /*
 Ejemplos de pedidos:
 
+Este es un pedido muy grande:
 SELECT s.nombre_sabor AS "Sabor", e.nombre_especial AS "Sabor Especial", sa.nombre_salsa AS "Salsa", b.nombre_bocadillo AS "Bocadillo", c.nombre_cucurucho AS "Tipo de Cucurucho" FROM sabores s
 INNER JOIN inter_sabor_especial i_s_e ON i_s_e.sabor_id = s.sabor_id
 INNER JOIN especiales e ON e.especial_id = i_s_e.especial_id
@@ -120,11 +121,39 @@ INNER JOIN inter_cucurucho_especial icu ON icu.especial_id = e.especial_id
 INNER JOIN cucuruchos c ON c.cucurucho_id = icu.cucurucho_id
 WHERE s.sabor_id = 1 AND e.especial_id = 2 AND sa.salsa_id = 5 AND b.bocadillo_id = 3 AND c.cucurucho_id = 2
 
-SELECT s.nombre_sabor, sa.nombre_salsa, b.nombre_bocadillo
+1) Pedido de un kilo de Banana Split con Dulce de Leche en pote:
+SELECT s.nombre_sabor AS "Sabor", c.nombre_cucurucho AS "Cucurucho"
 FROM sabores s
-INNER JOIN inter_sabor_salsa iesa ON iesa.sabor_id = s.sabor_id
+CROSS JOIN cucuruchos c
+WHERE s.nombre_sabor IN ('Banana Split', 'Dulce de Leche')
+AND c.nombre_cucurucho = 'Pote de 1Kg';
+
+2) Total vendido por cada empleado:
+SELECT e.nombre_empleado, e.apellido_empleado, SUM(p.monto) AS total_vendido
+FROM pagos p
+INNER JOIN empleados e ON p.empleado_id = e.empleado_id
+GROUP BY e.empleado_id;
+
+3) Todos los sabores disponibles en la tienda:
+SELECT s.sabor_id, s.nombre_sabor
+FROM sabores s
+WHERE s.tienda_id = 1;
+
+4) Pedido de una capelina con tramontana, salsa de chocolate y rocklets
+SELECT 
+  e.nombre_especial AS "Especial",
+  sa.nombre_salsa AS "Salsa",
+  b.nombre_bocadillo AS "Bocadillo",
+  c.nombre_cucurucho AS "Cucurucho"
+FROM especiales e
+INNER JOIN inter_especial_salsa iesa ON iesa.especial_id = e.especial_id
 INNER JOIN salsas sa ON sa.salsa_id = iesa.salsa_id
-INNER JOIN inter_sabor_bocadillo isb ON isb.sabor_id = s.sabor_id
-INNER JOIN bocadillos b ON b.bocadillo_id = isb.bocadillo_id
-WHERE s.sabor_id = 1;
+INNER JOIN inter_especial_bocadillo ieb ON ieb.especial_id = e.especial_id
+INNER JOIN bocadillos b ON b.bocadillo_id = ieb.bocadillo_id
+INNER JOIN inter_cucurucho_especial ice ON ice.especial_id = e.especial_id
+INNER JOIN cucuruchos c ON c.cucurucho_id = ice.cucurucho_id
+WHERE e.nombre_especial = 'Tramontana'
+  AND sa.nombre_salsa = 'Salsa de Chocolate'
+  AND b.nombre_bocadillo = 'Rocklets'
+  AND c.nombre_cucurucho = 'Capelina';
 */
